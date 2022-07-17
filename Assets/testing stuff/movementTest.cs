7777e9c25368670af35fadc1e181fe770ee8be30
239developer <factoriogmer@gmail.com>
2022-07-17 08:04:52 +0300

    testing stuff

---
+++ Assets/testing stuff/movementTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementTest : MonoBehaviour
+{
+    public float speed = 1.25f, rotSpeed = 2f;
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Input.GetAxis("Vertical"), 0f, 0f);
+        transform.Rotate(0f, 0f, Input.GetAxis("Mouse X") * rotSpeed);
+
+        Debug.Log(Input.GetAxis("Vertical"));
+    }
+}

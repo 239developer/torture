6d60d3f90b96b5f91751cea52f777bf2014319bc
239developer <factoriogmer@gmail.com>
2022-08-26 17:33:49 +0300

    loading screen

---
+++ Assets/Scripts/Menu_UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_UI : MonoBehaviour
+{
+    [SerializeField] public int loadingScreenScene = 0; 
+
+    public void ChangeScene(int scene)
+    {
+        SceneLoader.sceneToLoad = scene;
+        SceneManager.LoadScene(loadingScreenScene);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Image _progressBar;
+    public static int sceneToLoad = 1;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
+        operation.allowSceneActivation = false;
+
+        while(!operation.isDone)
+        {
+            _progressBar.fillAmount = operation.progress;
+
+            if(operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+            yield return null;
+        } 
+    }
+}
